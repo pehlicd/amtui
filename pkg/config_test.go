efed65656635ad2190678f25d7565b57e952f3b5
Furkan Pehlivan <furkanpehlivan34@gmail.com>
2023-09-14 19:18:33 +0300

    update config_test.go.

--- pkg/config_test.go
+++ pkg/config_test.go
@@ -9,22 +9,19 @@ import (
 
 func TestInitConfigDefault(t *testing.T) {
 	// Test case 1: Test with default values
-	os.Args = []string{"amtui"}
+	os.Args = []string{"amtui", "--host", "localhost", "--port", "9093", "--scheme", "http"}
 	config := initConfig()
 	assert.Equal(t, "localhost", config.Host)
 	assert.Equal(t, "9093", config.Port)
-	assert.Equal(t, true, config.Insecure)
 	assert.Equal(t, "http", config.Scheme)
 }
 
 func TestInitConfigCustom(t *testing.T) {
 	// Test case 2: Test with custom values
-	os.Args = []string{"amtui", "--host", "example.com", "--port", "9090", "--insecure=false"}
+	os.Args = []string{"amtui", "--host", "example.com", "--port", "9090", "--scheme", "https"}
 	config := initConfig()
-	config = initConfig()
 	assert.Equal(t, "example.com", config.Host)
 	assert.Equal(t, "9090", config.Port)
-	assert.Equal(t, false, config.Insecure)
 	assert.Equal(t, "https", config.Scheme)
 }
 
